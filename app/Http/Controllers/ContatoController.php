f289dab29f45c4b7fb5269723d54250dd37adb0e
Pedro Henrique Gomes dos Santos <peuh1997@gmail.com>
2021-06-23 02:41:13 -0300

    Update ContatoController.php

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,27 +9,27 @@ class ContatoController extends Controller
 {
     public function index(){
         $contatos = [
-           (Object)["nome"=>"Seth Gamble", "tel"=>"12345678"],
-           (Object)["nome"=>"Bruce Venture", "tel"=>"12345678"],
+           (Object)["nome"=>"Seth Wilson", "tel"=>"12345678"],
+           (Object)["nome"=>"Bruce Morris", "tel"=>"12345678"],
            (Object)["nome"=>"James Deen", "tel"=>"12345678"],
-           (Object)["nome"=>"Alex D.", "tel"=>"12345678"],
-           (Object)["nome"=>"Chris Diamond", "tel"=>"12345678"],
-           (Object)["nome"=>"Danny Wylde", "tel"=>"12345678"],
-           (Object)["nome"=>"Dennis Reed", "tel"=>"12345678"],
-           (Object)["nome"=>"Johnny Rapid", "tel"=>"12345678"],
-           (Object)["nome"=>"Jordi El Niño Polla", "tel"=>"12345678"],
-           (Object)["nome"=>"Rafael Alencar", "tel"=>"12345678"],
+           (Object)["nome"=>"Alex R.", "tel"=>"12345678"],
+           (Object)["nome"=>"Chris Steinfield", "tel"=>"12345678"],
+           (Object)["nome"=>"Maggie Wylde", "tel"=>"12345678"],
+           (Object)["nome"=>"Dennis Jhonson", "tel"=>"12345678"],
+           (Object)["nome"=>"Johnny Hitch", "tel"=>"12345678"],
+           (Object)["nome"=>"Jordan M.", "tel"=>"12345678"],
+           (Object)["nome"=>"Rafael Silva", "tel"=>"12345678"],
            (Object)["nome"=>"Kyle Dean", "tel"=>"12345678"],
-           (Object)["nome"=>"Bo Sinn", "tel"=>"12345678"],
-           (Object)["nome"=>"Arad Winwin", "tel"=>"12345678"],
-           (Object)["nome"=>"Armond Rizzo", "tel"=>"12345678"],
-           (Object)["nome"=>"Kris Evans", "tel"=>"12345678"],
-           (Object)["nome"=>"Kevin Warhol", "tel"=>"12345678"],
-           (Object)["nome"=>"Zane Williams", "tel"=>"12345678"],
+           (Object)["nome"=>"Bo Burnham", "tel"=>"12345678"],
+           (Object)["nome"=>"Alex Maeson", "tel"=>"12345678"],
+           (Object)["nome"=>"Claudia Rizzo", "tel"=>"12345678"],
+           (Object)["nome"=>"Kristina Souza", "tel"=>"12345678"],
+           (Object)["nome"=>"Kevin Page", "tel"=>"12345678"],
+           (Object)["nome"=>"Zane Silverstein", "tel"=>"12345678"],
            (Object)["nome"=>"Blake Mitchell", "tel"=>"12345678"],
-           (Object)["nome"=>"Manuel Ferrara", "tel"=>"12345678"],
-           (Object)["nome"=>"Jack Harrer", "tel"=>"12345678"],
-           (Object)["nome"=>"Tyler Nixon", "tel"=>"12345678"]
+           (Object)["nome"=>"Manuel Carlos", "tel"=>"12345678"],
+           (Object)["nome"=>"Jack Willis", "tel"=>"12345678"],
+           (Object)["nome"=>"Tyler Brown", "tel"=>"12345678"]
         ];
         
         $contato = new Contato();
